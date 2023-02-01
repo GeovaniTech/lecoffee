6d670dd6d2cd197d13b1e088e4db5fffa2dfb73b
Geovani Debastiani <geovanidebastiani32@gmail.com>
2023-02-01 15:57:28 -0300

    Fixing name page

--- src/main/webapp/resources/js/menu.js
+++ src/main/webapp/resources/js/menu.js
@@ -2,12 +2,12 @@ function openMenu() {
     if(document.getElementById("header").style.width == "0px") {
         document.getElementById("header").style.left = "0px";
         document.getElementById("header").style.width = "250px";
-        document.getElementById("body-main").style.marginLeft = "250px"
-        document.getElementById("top-header").style.paddingLeft = "250px"
+        document.getElementById("body-main").style.marginLeft = "260px"
+        document.getElementById("top-header").style.paddingLeft = "260px"
     } else {
         document.getElementById("header").style.left = "-300px";
         document.getElementById("header").style.width = "0px";
-        document.getElementById("body-main").style.marginLeft = "0px"
+        document.getElementById("body-main").style.marginLeft = "10px"
         document.getElementById("top-header").style.paddingLeft = "0px"
     }
 }
