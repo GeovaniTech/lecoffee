5bc2ffb95c037d9e680be30e3e532791ba776a4f
Geovani Debastiani <geovanidebastiani32@gmail.com>
2023-02-11 12:10:45 -0300

    Layout dialog finished

---
+++ src/main/java/managedBean/MBProduct.java
@@ -0,0 +1,13 @@
+package managedBean;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import utils.AbstractBean;
+
+@Named("MBProduct")
+@ViewScoped
+public class MBProduct extends AbstractBean {
+	private static final long serialVersionUID = -7828714455689754401L;
+	
+	
+}
