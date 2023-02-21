a4f3b916e3d18da72eb70a2eb317f697e37a9830
Geovani Debastiani <geovanidebastiani32@gmail.com>
2023-02-21 19:40:00 -0300

    Removing cookies

--- src/main/java/managedBean/MBAppConfigs.java
+++ src/main/java/managedBean/MBAppConfigs.java
@@ -11,9 +11,7 @@ import javax.enterprise.context.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import utils.ImageUtil;
 
@@ -27,8 +25,6 @@ public class MBAppConfigs implements Serializable {
 	private String language;
 	
 	private ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-	private HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
-	private HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
 
 	public MBAppConfigs() {
 		this.localeList = new ArrayList<Locale>();
@@ -37,17 +33,6 @@ public class MBAppConfigs implements Serializable {
 		
 		language = Locale.getDefault().getLanguage();
 		
-		String cookieDark = getCookieDarkMode();
-		
-		if(cookieDark != null && cookieDark.equals("false")) {
-			darkMode = false;
-		} else if (cookieDark != null) { 
-			darkMode = true;
-		} else {
-			Cookie cookieDarkMode = new Cookie("darkMode", "false");
-			cookieDarkMode.setMaxAge(Integer.MAX_VALUE);
-			response.addCookie(cookieDarkMode);
-		}
 	}
 	
 	public String getBrazilianCurrency(Double value) {
@@ -65,36 +50,6 @@ public class MBAppConfigs implements Serializable {
 	public void refreshPage() throws IOException {
 	    ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
 	    ec.redirect(((HttpServletRequest) ec.getRequest()).getRequestURI());
-	}
-	
-	public String getCookieDarkMode() {
-		Cookie[] cookies = request.getCookies();
-		if (cookies != null) {
-		    for (Cookie cookie : cookies) {
-		        if (cookie.getName().equals("darkMode")) {
-		            return cookie.getValue();
-		        } 
-		    }
-		}
-		
-		return null;
-	}
-	
-	public boolean changeCookieDarkMode() {
-		Cookie[] cookies = request.getCookies();
-		if (cookies != null) {
-		    for (Cookie cookie : cookies) {
-		        if (cookie.getName().equals("darkMode")) {
-		            cookie.setValue("" + darkMode); // atualiza o valor do cookie
-		            cookie.setMaxAge(Integer.MAX_VALUE);
-		            response.addCookie(cookie); // envia o cookie de volta para o navegador
-		            
-		            return true;
-		        }
-		    }
-		}
-		
-		return false;
 	}
 	
 	public boolean isDarkMode() {
@@ -127,21 +82,5 @@ public class MBAppConfigs implements Serializable {
 
 	public void setExternalContext(ExternalContext externalContext) {
 		this.externalContext = externalContext;
-	}
-
-	public HttpServletRequest getRequest() {
-		return request;
-	}
-
-	public void setRequest(HttpServletRequest request) {
-		this.request = request;
-	}
-
-	public HttpServletResponse getResponse() {
-		return response;
-	}
-
-	public void setResponse(HttpServletResponse response) {
-		this.response = response;
 	}
 }
